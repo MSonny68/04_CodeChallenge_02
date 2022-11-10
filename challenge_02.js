f3c6f1021ed10898661931c8745de15e0fb10684
Maik Sonnenberg <maik.sonnenberg@hotmail.de>
2022-11-10 16:56:52 +0100

    mit Kommentar

--- challenge_02.js
+++ challenge_02.js
@@ -22,11 +22,11 @@ let yourName;
 yourName = prompt( "Bitte gib deinen Namen ein: ");
 alter = parseInt( prompt( "Bitte gib dein Alter an: " ) );
 
-if ( alter >= 0 && alter < 6 ) {
+if ( alter >= 0 && alter < 6 ) {   // Alter 0..5 ??
     getränk = "Milch";
-} else if ( alter >=6 && alter < 13 ){
+} else if ( alter >=6 && alter < 13 ){  // Alter 6..12?? 
     getränk = "Saft";
-} else if ( alter >= 13 && alter < 18 ){
+} else if ( alter >= 13 && alter < 18 ){  // Alter 13..17?? 
     getränk = "Cola";
 }
 
